024882eb3d3fecdf6265d3e1edec98f1f8edde08
Artur-Lepczynski <artur.lepczynski@gmail.com>
2022-10-19 19:03:59 +0200

    Improved calculator logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,26 @@
 const darkModeSwitch = document.querySelector("#dark-mode-switch");
 const darkModeSwitchHead = darkModeSwitch.nextElementSibling;
-// darkModeSwitch.checked = false; 
 
 darkModeSwitch.addEventListener("change", (event) => {
-    console.log("switched:", darkModeSwitch.checked);
+    //TODO: change page styling 
     if (darkModeSwitch.checked) {
         darkModeSwitchHead.style.left = "45px";
     } else {
         darkModeSwitchHead.style.left = "5px";
-    }
+    }; 
 });
 
 const calculator = document.querySelector("#calculator-wrapper");
 const calculatorButtons = document.querySelector("#calculator-buttons");
 const calculatorDisplay = document.querySelector("#calculator-display-text");
-const calculatorDisplayMaxLength = 16;
+const calculatorDisplayMaxLength = 14;
 const calculatorSecondaryDisplay = document.querySelector("#calculator-secondary-display-text");
 const calculatorSecondaryDisplayMaxLength = 25;
 
-let operand1;
-let operand2;
-let result = "";
+let operand1, operand2, result;
 const errorMessage = "Error";
 
-//add error mode!!!!!!!!!
-const modes = { none: "none", add: "add", subtract: "subtract", multiply: "multiply", divide: "divide" };
+const modes = { none: "none", error: "error", add: "add", subtract: "subtract", multiply: "multiply", divide: "divide" };
 Object.freeze(modes);
 let mode = modes.none;
 
@@ -36,14 +32,14 @@ calculatorButtons.addEventListener("click", (event) => {
     } else if (event.target.matches("#button-equals")) {
         handleResult();
     } else if (event.target.matches("#button-clear")) {
-        calculatorDisplay.textContent === "" ? clearAll() : calculatorDisplay.textContent = "";
+        handleClear();
     } else if (event.target.matches("#button-dot")) {
         handleDot();
     } else if (event.target.matches("#button-backspace")) {
         handleBackspace();
     } else if (event.target.matches("#button-sign")) {
         handleSignChange();
-    }
+    };
 });
 
 calculator.addEventListener("inputError", (event) => {
@@ -55,31 +51,31 @@ calculator.addEventListener("inputError", (event) => {
 });
 
 function handleNumberInput(event) {
+    let number = event.target.dataset.number; 
+
+    if (mode === modes.error) {
+        calculatorDisplay.textContent = "";
+        calculatorDisplay.textContent += number;
+        setMode(modes.none);
+        return;
+    };
+
     if (calculatorDisplay.textContent.length < calculatorDisplayMaxLength) {
-        if (calculatorDisplay.textContent !== errorMessage) {
-            if (event.target.textContent !== "0") {
-                if (calculatorDisplay.textContent === "0") {
-                    calculatorDisplay.textContent = "";
-                }
-                calculatorDisplay.textContent += event.target.textContent;
-            } else {
-                if (calculatorDisplay.textContent !== "0") {
-                    calculatorDisplay.textContent += event.target.textContent;
-                }
-            }
-        }else{
-            calculatorDisplay.textContent = "";
-            calculatorDisplay.textContent += event.target.textContent;
-        }
+        if (event.target.textContent !== "0") {
+            if (calculatorDisplay.textContent === "0") calculatorDisplay.textContent = "";
+            calculatorDisplay.textContent += number;
+        } else {
+            if (calculatorDisplay.textContent !== "0") calculatorDisplay.textContent += number;
+        };
+
     } else {
         calculator.dispatchEvent(new CustomEvent("inputError", { detail: "handleNumberInput" }));
-    }
+    };
 };
 
 function handleOperationInput(event) {
-    // console.log("handle operation, mode:", mode); 
 
-    if ((calculatorDisplay.textContent === "" && mode === modes.none) || (calculatorDisplay.textContent === errorMessage && mode === modes.none)) {
+    if (calculatorDisplay.textContent === "" && mode === modes.none || mode === modes.error) {
         calculator.dispatchEvent(new CustomEvent("inputError", { detail: "handleOperationInput" }));
         return;
     } else if (mode === modes.none) {
@@ -87,11 +83,10 @@ function handleOperationInput(event) {
         calculatorSecondaryDisplay.textContent = operand1;
         calculatorDisplay.textContent = "";
     } else if (mode !== modes.none && calculatorDisplay.textContent !== "") {
-        operand1 = handleResult(true);
+        handleResult(true);
     };
 
-    if (operand1 !== errorMessage) {
-        //TODO: add classes to highlight buttons
+    if (mode !== modes.error) {
         if (event.target.matches("#button-add")) {
             setMode(modes.add);
         } else if (event.target.matches("#button-subtract")) {
@@ -102,101 +97,108 @@ function handleOperationInput(event) {
             setMode(modes.divide);
         };
     }
-
-    // console.log("oper1:", operand1, "mode:", mode);
 };
 
 function handleResult(displayOnSecondary = false) {
 
-    if (calculatorDisplay.textContent === "" || mode === modes.none) {
-        calculator.dispatchEvent(new CustomEvent("inputError", { detail: "handleResult" }));
-        return;
-    }
+    if (calculatorDisplay.textContent === "" || mode === modes.none || mode === modes.error) return;
     operand2 = Number.parseFloat(calculatorDisplay.textContent);
-    // console.log("handle result, oper1:", operand1, "oper2:", operand2);
+    let temp = 0; 
 
-    //TODO: remove classes to highlight buttons
     switch (mode) {
         case modes.add:
-            result = Number.parseFloat((operand1 + operand2).toFixed(calculatorDisplayMaxLength));
+            // result = Number.parseFloat(temp.toFixed(calculatorDisplayMaxLength - ((""+Math.round(temp)).length+1)));
+            // result = Number.parseFloat((operand1 * operand2).toFixed(calculatorDisplayMaxLength));
+            temp = (operand1 + operand2);
             break;
         case modes.subtract:
-            result = Number.parseFloat((operand1 - operand2).toFixed(calculatorDisplayMaxLength));
+            temp = (operand1 - operand2);
             break;
         case modes.divide:
-            result = Number.parseFloat((operand1 / operand2).toFixed(calculatorDisplayMaxLength));
+            temp = (operand1 / operand2);
             break;
         case modes.multiply:
-            result = Number.parseFloat((operand1 * operand2).toFixed(calculatorDisplayMaxLength));
+            temp = (operand1 * operand2);
             break;
     };
 
-
-    console.log("RESULT:", result);
+    if(!Number.isInteger(temp)){
+        result = Number.parseFloat(temp.toFixed(calculatorDisplayMaxLength - ((""+Math.round(temp)).length+1)));
+    }else{
+        result = temp; 
+    };
 
     if (result === Infinity || result === -Infinity || isNaN(result) || result.toString().length > calculatorDisplayMaxLength) {
-        setMode(modes.none);
         calculatorDisplay.textContent = errorMessage;
         calculatorSecondaryDisplay.textContent = "";
-        return errorMessage;
+        setMode(modes.error);
+        return;
     };
 
     if (displayOnSecondary) {
         calculatorSecondaryDisplay.textContent = result;
         calculatorDisplay.textContent = "";
-        return result;
     } else {
         calculatorDisplay.textContent = result;
         calculatorSecondaryDisplay.textContent = "";
         setMode(modes.none);
-    }
-
+    };
 };
 
 function setMode(newMode) {
-    //remove styling
+    //TODO: add and remove styling
     mode = newMode;
     console.log("set mode:", mode);
-}
+};
 
-function clearAll() {
-    //TODO: remove style from operation buttons
-    calculatorSecondaryDisplay.textContent = "";
-    setMode(modes.none);
+function handleClear() {
+    if (calculatorDisplay.textContent === "") {
+        calculatorSecondaryDisplay.textContent = "";
+        setMode(modes.none);
+    } else {
+        if(mode === modes.error) setMode(modes.none); 
+        calculatorDisplay.textContent = "";
+    }
 };
 
 function handleDot() {
-    if (calculatorDisplay.textContent === "") {
-        calculatorDisplay.textContent = "0."
-    } else if (calculatorDisplay.textContent.endsWith("0.")) {
+    if(mode === modes.error){
+        calculatorDisplay.textContent = "0.";
+        setMode(modes.none);
+    } else if (calculatorDisplay.textContent === "") {
+        calculatorDisplay.textContent = "0.";
+    } else if (calculatorDisplay.textContent === "0.") {
         calculatorDisplay.textContent = "";
     } else if (calculatorDisplay.textContent.endsWith(".")) {
         calculatorDisplay.textContent = calculatorDisplay.textContent.slice(0, calculatorDisplay.textContent.length - 1);
     } else if (!calculatorDisplay.textContent.endsWith(".") && !calculatorDisplay.textContent.split("").includes(".")) {
-        calculatorDisplay.textContent += "."
+        calculatorDisplay.textContent += ".";
     };
-};
+}
 
 function handleSignChange() {
-    if (calculatorDisplay.textContent !== "") {
+    if (calculatorDisplay.textContent !== "" && mode !== modes.error) {
         if (calculatorDisplay.textContent.startsWith("-")) {
             calculatorDisplay.textContent = calculatorDisplay.textContent.slice(1, calculatorDisplay.textContent.length);
         } else {
             calculatorDisplay.textContent = "-" + calculatorDisplay.textContent;
         };
+    }else{
+        calculator.dispatchEvent(new CustomEvent("inputError", { detail: "handleSignChange" }));
     };
 };
 
 function handleBackspace() {
-    if(calculatorDisplay.textContent === errorMessage){
+    if (mode === modes.error) {
         calculatorDisplay.textContent = "";
-    }else {
+        setMode(modes.none);
+    } else {
         if (calculatorDisplay.textContent.startsWith("-") && calculatorDisplay.textContent.length === 2) {
             calculatorDisplay.textContent = "";
         } else {
             calculatorDisplay.textContent = calculatorDisplay.textContent.slice(0, calculatorDisplay.textContent.length - 1);
         };
-    }
+    };
 };
 
 
