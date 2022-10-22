151bcb5178d9218079764425bceccf7d84f59af0
Artur-Lepczynski <artur.lepczynski@gmail.com>
2022-10-22 21:50:47 +0200

    fix: correct representation of small numbers
    Small numbers will display in decimal notation instead of scientific notation

--- scripts/main.js
+++ scripts/main.js
@@ -27,32 +27,39 @@ let mode = modes.none;
 calculatorButtons.addEventListener("click", (event) => {
     if (event.target.matches(".button-number")) {
         handleNumberInput(event);
-    } else if (event.target.matches(".button-operation")) {
+    } else if (event.target.matches(".button-operation, .button-operation > i")) {
         handleOperationInput(event);
-    } else if (event.target.matches("#button-equals")) {
+    } else if (event.target.matches("#button-equals, #button-equals > i")) {
         handleResult(false, true);
     } else if (event.target.matches("#button-clear")) {
         handleClear();
     } else if (event.target.matches("#button-dot")) {
         handleDot();
-    } else if (event.target.matches("#button-backspace")) {
+    } else if (event.target.matches("#button-backspace, #button-backspace > i")) {
         handleBackspace();
-    } else if (event.target.matches("#button-sign")) {
+    } else if (event.target.matches("#button-sign, #button-sign > i")) {
         handleSignChange();
     };
+    handlecalculatorDisplayFontSizeChange();
 });
 
 calculator.addEventListener("inputError", (event) => {
+    console.log("error", event.detail);
     calculator.style.borderColor = "red";
     setTimeout(() => {
         calculator.style.borderColor = "";
     }, 100);
 });
 
+function handlecalculatorDisplayFontSizeChange() {
+    let length = calculatorDisplay.textContent.length;
+    calculatorDisplay.style.fontSize = length > 10 ? "40px" : "48px";
+};
+
 function handleNumberInput(event) {
     let number = event.target.dataset.number;
 
-    if(continued || mode === modes.error) clearAll(); 
+    if (continued || mode === modes.error) clearAll();
 
     if (calculatorDisplay.textContent.length < calculatorDisplayMaxLength) {
         if (number !== "0") {
@@ -80,34 +87,34 @@ function handleOperationInput(event) {
     };
 
     if (mode !== modes.error) {
-        if (event.target.matches("#button-add")) {
+        if (event.target.matches("#button-add, #button-add > i")) {
             setMode(modes.add);
-        } else if (event.target.matches("#button-subtract")) {
+        } else if (event.target.matches("#button-subtract, #button-subtract > i")) {
             setMode(modes.subtract);
-        } else if (event.target.matches("#button-multiply")) {
+        } else if (event.target.matches("#button-multiply, #button-multiply > i")) {
             setMode(modes.multiply);
-        } else if (event.target.matches("#button-divide")) {
+        } else if (event.target.matches("#button-divide, #button-divide > i")) {
             setMode(modes.divide);
         };
     };
 };
 
-let continued = false; 
+let continued = false;
 function handleResult(displayOnSecondary = false, trueClick = false) {
 
     let temp = 0;
     let symbol;
 
-    if (calculatorDisplay.textContent === "" || mode === modes.none || mode === modes.error){
+    if (calculatorDisplay.textContent === "" || mode === modes.none || mode === modes.error) {
         calculator.dispatchEvent(new CustomEvent("inputError", { detail: "handleResult" }));
         return;
     };
 
-    if(displayOnSecondary && continued){
+    if (displayOnSecondary && continued) {
         operand1 = Number.parseFloat(calculatorDisplay.textContent);
         calculatorSecondaryDisplay.textContent = operand1;
         calculatorDisplay.textContent = "";
-        continued = false; 
+        continued = false;
         return;
     };
 
@@ -132,8 +139,22 @@ function handleResult(displayOnSecondary = false, trueClick = false) {
             break;
     };
 
+    //handle large and small real numbers, + scientific notation
     if (!Number.isInteger(temp)) {
-        result = Number.parseFloat(temp.toFixed(calculatorDisplayMaxLength - (("" + Math.round(temp)).length + 1)));
+        let rounded = Math.round(temp) + "";
+        result = rounded.length >= calculatorDisplayMaxLength ? Number.parseInt(rounded) : Number.parseFloat(temp.toFixed(calculatorDisplayMaxLength - (rounded.length + 1)));
+        if(result.toString().includes("e-")){
+            let exponent = Number.parseInt(result.toString().split("e-")[1]);
+            let baseString = result.toString().split("e-")[0];
+            let base =  baseString.indexOf(".") === -1 ? baseString.length : baseString.length - 1;  
+            result = result.toFixed(exponent + base - 1);
+        };
+        if(result === 0){
+            calculatorDisplay.textContent = errorMessage;
+            calculatorSecondaryDisplay.textContent = "";
+            setMode(modes.error);
+            return;
+        };
     } else {
         result = temp;
     };
@@ -160,10 +181,11 @@ function handleResult(displayOnSecondary = false, trueClick = false) {
 function setMode(newMode) {
     //TODO: add and remove styling
     mode = newMode;
+    console.log("mode change:", mode);
 };
 
 function handleClear() {
-    if(continued){
+    if (continued) {
         clearAll();
     } else if (calculatorDisplay.textContent === "") {
         calculatorSecondaryDisplay.textContent = "";
@@ -187,14 +209,14 @@ function handleDot() {
         calculatorDisplay.textContent = calculatorDisplay.textContent.slice(0, calculatorDisplay.textContent.length - 1);
     } else if (!calculatorDisplay.textContent.split("").includes(".")) {
         calculatorDisplay.textContent += ".";
-    }else{
+    } else {
         calculator.dispatchEvent(new CustomEvent("inputError", { detail: "handleDot" }));
     };
 };
 
 function handleSignChange() {
     if (calculatorDisplay.textContent !== "" && mode !== modes.error) {
-        if(continued){
+        if (continued) {
             calculatorSecondaryDisplay.textContent = "";
             continued = false;
             setMode(modes.none);
@@ -214,7 +236,7 @@ function handleBackspace() {
         calculatorDisplay.textContent = "";
         setMode(modes.none);
     } else {
-        if(continued){
+        if (continued) {
             continued = false;
             setMode(modes.none);
         };
@@ -222,15 +244,15 @@ function handleBackspace() {
         if (calculatorDisplay.textContent.startsWith("-") && calculatorDisplay.textContent.length === 2) {
             calculatorDisplay.textContent = "";
         } else {
-            calculatorDisplay.textContent = calculatorDisplay.textContent.slice(0, calculatorDisplay.textContent.length - 1); 
+            calculatorDisplay.textContent = calculatorDisplay.textContent.slice(0, calculatorDisplay.textContent.length - 1);
         };
     };
 };
 
-function clearAll(){
+function clearAll() {
     calculatorSecondaryDisplay.textContent = "";
     calculatorDisplay.textContent = "";
     operand2 = undefined;
-    continued = false; 
+    continued = false;
     setMode(modes.none);
 };
